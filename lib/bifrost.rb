112cbf09b30e830955b6f5c0cc10bb8b189b42b0
Shirren Premaratne <shirren@me.com>
2016-11-02 09:14:16 +1100

    Added a module level feature to set the logger

--- lib/bifrost.rb
+++ lib/bifrost.rb
@@ -4,9 +4,17 @@ require 'bifrost/topic'
 require 'bifrost/subscriber'
 require 'bifrost/manager'
 require 'bifrost/worker'
+require 'celluloid'
 
 # Bifrost is a pub/sub gem built on top of the Azure MessageBus system
 module Bifrost
+
+  # Workers and the infrastructure can log using the standard level of granularity affored to any
+  # standard logger (i.e. info, debug, error levels etc)
+  def self.logger=(log_provider)
+    Celluloid.logger = log_provider
+  end
+
   # Simple utlity that creates a topic and a single subscriber for the given
   # topic. The topic is returned
   def self.create_topic_with_subscriber(topic, subscriber)
